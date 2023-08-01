6889ae426deb3a5b45293b253b06f3dd08d0f4eb
iulicc <ioaniulian.u@gmail.com>
2023-08-01 15:30:27 +0300

    implementing viewstate to calculate body w && h

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,19 @@
+function get_cookie (cookie_name) {
+    const cookies = document.cookie.split(';')
+    for (let cookie of cookies) {
+        cookie = cookie.trim()
+        if (cookie.length > 0) {
+            let name, value;
+            [name, value] = cookie.split('=')
+            if (name === cookie_name) return value
+        }
+    }
+    return null
+}
 
+const VIEWSTATE = parseInt(get_cookie('viewstate')) || 640
+document.body.style.width = VIEWSTATE + 'px'
+document.body.style.height = VIEWSTATE + 'px'
 
 // Temps update
 window.nzxt = {
